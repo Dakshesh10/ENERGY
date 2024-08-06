5e7c2fa440a00cf5094bac66299b26ed1481a1d5
Dakshesh <dakshesh1010@gmail.com>
2024-08-07 02:13:53 +0530

    Biuld settings changes

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        Application.targetFrameRate = 60;
         if (Instance == null)
         {
             Instance = this;
@@ -23,6 +24,7 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
     }
 
     private void Start()
